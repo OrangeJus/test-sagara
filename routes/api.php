c028181164691d4940bb3f2fb62b0d6893ace757
OrangeJus <balurana.baau@gmail.com>
2023-08-24 18:26:25 +0800

    Membuat API awal

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::get('/hay', function(){
+//     return 'Hello world';
+// });
+
+// Route::get('products', [ProductController::class, 'index']);
+// Route::post('products', [ProductController::class, 'store']);
+
+// Jika hanya ingin membuat aplikasi biasa kita bisa menggunakan resource
+Route::resource('products', ProductController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
